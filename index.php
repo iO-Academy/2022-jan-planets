1883fc740d25e22cea09689022d28cd1e90bb4c6
Charlotte Buckley <cfbuckley14@gmail.com>
2022-03-21 15:06:45 +0000

    hardcoded html, added normalize and set up Sass

--- index.php
+++ index.php
@@ -3,12 +3,26 @@
 <html lang="en-GB">
 <head>
     <title>Planet Plinky Plonk</title>
+    <link rel="stylesheet" type="text/css" href="normalize.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:ital,wght@1,100&display=swap" rel="stylesheet">
 </head>
 <body>
 <header>
-
+    <h1>Planet Plinky Plonk</h1>
+    <svg>SUN*****</svg>
 </header>
 <main>
+    <div class="planet-card-container">
+            <div class="planet-card">
+                <img src="earth.png" alt="Earth"/>
+                <div class="planet-card-info-container">
+                    <h2>Earth</h2>
+                    <h3>Planet Type: Earth</h3>
+                </div>
+            </div>
+    </div>
 
 </main>
 </body>
