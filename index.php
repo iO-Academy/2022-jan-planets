d8ccd05f07107c2bb12523aef54e248b2c633f5b
oritchie77 <96186469+oritchie77@users.noreply.github.com>
2022-03-22 15:00:44 +0000

    completed styling for solar system, added if statments for ringed plannets svgs

--- index.php
+++ index.php
@@ -28,29 +28,31 @@ $allPlanets = PlanetHydrator::getPlanets($db);
 </head>
 <body>
 <main>
+    <div class="title-image-container">
     <img id="title-image" src="planet-plinky-plonk.png" alt="Planet Plinky Plonk"/>
+    </div>
     <img class="sun-image mobile" src="sun.png" alt="Sun"/>
 
     <h1>Planet Plinky Plonk</h1>
     <div class="solar-system-container">
         <img class="sun-image desktop" src="sun.png" alt="Sun"/>
-        <?= OrbitSvg::generateOrbitSvg('48%', '98%', 'senda', '1%'); ?>
-        <?= OrbitSvg::generateOrbitSvg('46%', '4%', 'eris', '1%'); ?>
-        <?= OrbitSvg::generateOrbitSvg('44%', '94%', 'gonggong', '1%'); ?>
-        <?= OrbitSvg::generateOrbitSvg('42%', '8%', 'makemake', '1%'); ?>
-        <?= OrbitSvg::generateOrbitSvg('38%', '88%', 'quaoar', '1%'); ?>
-        <?= OrbitSvg::generateOrbitSvg('36%', '12%', 'haumea', '1%'); ?>
-        <?= OrbitSvg::generateOrbitSvg('34%', '84%', 'pluto', '1%'); ?>
-        <?= OrbitSvg::generateOrbitSvg('32%', '16%', 'orcus', '1%'); ?>
-        <?= OrbitSvg::generateOrbitSvg('30%', '80%', 'neptune', '1%'); ?>
-        <?= OrbitSvg::generateOrbitSvg('28%', '20%', 'uranus', '1%'); ?>
-        <?= OrbitSvg::generateOrbitSvg('26%', '76%', 'saturn', '1%'); ?>
-        <?= OrbitSvg::generateOrbitSvg('24%', '24%', 'jupiter', '1%'); ?>
-        <?= OrbitSvg::generateOrbitSvg('22%', '72%', 'ceres', '1%'); ?>
-        <?= OrbitSvg::generateOrbitSvg('16%', '34%', 'mars', '1%'); ?>
-        <?= OrbitSvg::generateOrbitSvg('14%', '64%', 'earth', '1%'); ?>
-        <?= OrbitSvg::generateOrbitSvg('12%', '38%', 'venus', '1%'); ?>
-        <?= OrbitSvg::generateOrbitSvg('10%', '60%', 'mercury', '1%'); ?>
+        <?= OrbitSvg::generateOrbitSvg('48%', '98%', 'senda', '1'); ?>
+        <?= OrbitSvg::generateOrbitSvg('46%', '4%', 'eris', '1'); ?>
+        <?= OrbitSvg::generateOrbitSvg('44%', '94%', 'gonggong', '1'); ?>
+        <?= OrbitSvg::generateOrbitSvg('42%', '8%', 'makemake', '1'); ?>
+        <?= OrbitSvg::generateOrbitSvg('38%', '88%', 'quaoar', '1'); ?>
+        <?= OrbitSvg::generateOrbitSvg('36%', '12%', 'haumea', '1'); ?>
+        <?= OrbitSvg::generateOrbitSvg('34%', '84%', 'pluto', '1'); ?>
+        <?= OrbitSvg::generateOrbitSvg('32%', '16%', 'orcus', '1'); ?>
+        <?= OrbitSvg::generateOrbitSvg('30%', '80%', 'neptune', '2'); ?>
+        <?= OrbitSvg::generateOrbitSvg('28%', '20%', 'uranus', '2'); ?>
+        <?= OrbitSvg::generateOrbitSvg('25%', '75%', 'saturn', '2'); ?>
+        <?= OrbitSvg::generateOrbitSvg('22%', '28%', 'jupiter', '4'); ?>
+        <?= OrbitSvg::generateOrbitSvg('18%', '68%', 'ceres', '1'); ?>
+        <?= OrbitSvg::generateOrbitSvg('16%', '34%', 'mars', '1.6'); ?>
+        <?= OrbitSvg::generateOrbitSvg('14%', '64%', 'earth', '2'); ?>
+        <?= OrbitSvg::generateOrbitSvg('12%', '38%', 'venus', '1.4'); ?>
+        <?= OrbitSvg::generateOrbitSvg('10%', '60%', 'mercury', '1'); ?>
     </div>
     <div class="planet-card-container">
     <?php
--- src/OrbitSvg.php
+++ src/OrbitSvg.php
@@ -5,13 +5,36 @@ namespace planetApp;
 class OrbitSvg
 {
 
-
-    public static function generateOrbitSvg(string $orbitRadius, string $planetPosition, string $planetName, string $planetRadius): string
+    public static function generateOrbitSvg(string $orbitRadius, string $planetPosition, string $planetName, float $planetRadius): string
     {
-        return '<svg class="orbit-svg" width="100%" height="100%">' .
-            '<ellipse  class=orbit id="' . $planetName .  '-orbit" cx="50%" cy="50%" rx="' . $orbitRadius . '" ry="' . $orbitRadius . '" stroke="white" stroke-width="1" fill="none"/>' .
-                '<circle id="' . $planetName . '" r="' . $planetRadius . '" cx="' . $planetPosition . '" cy="50%" stroke="none" stroke-width="1"/>' .
-        '</svg>';
+        if($planetName === 'saturn') {
+            $ringOneRadius = $planetRadius + 0.5;
+            $ringTwoRadius = $planetRadius + 0.2;
+
+            return
+                '<svg class="orbit-svg " width="100%" height="100%">' .
+                '<ellipse  class=orbit id="' . $planetName . '-orbit" cx="50%" cy="50%" rx="' . $orbitRadius . '" ry="' . $orbitRadius . '" stroke="white" stroke-width="1" fill="none"/>' .
+                '<circle id="' . $planetName . '" r="' . $planetRadius . '%" cx="' . $planetPosition . '" cy="50%" stroke="none" stroke-width="1"/>' .
+                '<ellipse class="' . $planetName . '-ring" cx="' . $planetPosition . '" cy="50%" rx="' . $ringOneRadius . '%" ry="' . $planetRadius . '%" fill="none" stroke="red" stroke-width="3"></ellipse>' .
+                '<ellipse class="' . $planetName . '-ring" cx="' . $planetPosition . '" cy="50%" rx="' . $ringTwoRadius . '%" ry="' . $planetRadius . '%" fill="none" stroke="red" stroke-width="3"></ellipse>' .
+                '</svg>';
+
+        } elseif ($planetName === 'haumea') {
+            $ringOneRadius = $planetRadius + 0.5;
+            $humeaWidth = $planetRadius + 0.5;
+
+            return
+                '<svg class="orbit-svg" width="100%" height="100%">' .
+                '<ellipse  class=orbit id="' . $planetName . '-orbit" cx="50%" cy="50%" rx="' . $orbitRadius . '" ry="' . $orbitRadius . '" stroke="white" stroke-width="1" fill="none"/>' .
+                '<ellipse id="' . $planetName . '" cx="' . $planetPosition . '" cy="50%" rx="' . $planetRadius . '%" ry="' . $humeaWidth . '%" fill="none"></ellipse>' .
+                '<ellipse class="' . $planetName . '-ring" cx="' . $planetPosition . '" cy="50%" rx="' . $ringOneRadius . '%" ry="' . $planetRadius . '%" fill="none" stroke="red" stroke-width="3"></ellipse>' .
+                '</svg>';
+        } else {
+            return '<svg class="orbit-svg" width="100%" height="100%">' .
+                '<ellipse  class=orbit id="' . $planetName . '-orbit" cx="50%" cy="50%" rx="' . $orbitRadius . '" ry="' . $orbitRadius . '" stroke="white" stroke-width="1" fill="none"/>' .
+                '<circle class="planet-svg" id="' . $planetName . '" r="' . $planetRadius . '%" cx="' . $planetPosition . '" cy="50%" stroke="none" stroke-width="1"/>' .
+                '</svg>';
+        }
     }
 
 }
