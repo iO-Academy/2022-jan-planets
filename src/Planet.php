959e9e398e32f44d55967e223a5d174805e81252
“Maxwell <newton.maxwell@gmail.com>
2022-03-22 13:50:15 +0000

    fixed tests and removed vendor folder from commit

--- src/Planet.php
+++ src/Planet.php
@@ -8,6 +8,21 @@ class Planet
     protected $image_src;
     protected $type;
 
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function setImageSrc(string $image_src): void
+    {
+        $this->image_src = $image_src;
+    }
+
+    public function setType(string $type): void
+    {
+        $this->type = $type;
+    }
+
     public function createCardHtml(): string
     {
         return('
