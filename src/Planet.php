160c2705892e81cef170251e4b6d6da77a13f86c
Charlotte Buckley <cfbuckley14@gmail.com>
2022-03-22 10:53:44 +0000

    updates to image sizing

--- src/Planet.php
+++ src/Planet.php
@@ -24,8 +24,8 @@ class Planet
         return('
         <div class="planet-card">
             <img src="'.$this->image_src.'">
+            <img src="earth.png" alt="Earth"/>
             <div class="planet-card-info-container">
-                <img src="earth.png" alt="Earth"/>
                 <h2>'.$this->name.'</h2>
                 <h3>'.$this->type.'</h3>
             </div>
